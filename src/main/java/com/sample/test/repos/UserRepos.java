018ca056a9ea9546ffd20a400786728ce9b38a0a
Gershon <gershon.s.mabuang@accenture.com>
2014-02-07 14:35:53 +0800

    Testing commit

--- src/main/java/com/sample/test/repos/UserRepos.java
+++ src/main/java/com/sample/test/repos/UserRepos.java
@@ -15,8 +15,8 @@ public interface UserRepos extends JpaRepository<User, Long>
 	public List<User> findByUsernameOrPasswordOrUsernameLikeOrPasswordLikeOrUsernameStartingWithOrPasswordStartingWithOrUsernameEndingWithOrPasswordEndingWithOrUsernameContainingOrPasswordContaining
 	(String userName, String passWord, String userName1, String passWord1, String userName11, 
 	 String passWord11, String userName111, String passWord111, String userName1111, String passWord1111);
-	/*public List<User> findByUsernameOrPasswordLike(String userName, String passWord);
-	public List<User> findByUsernameStartingWith(String userName);
+	public List<User> findByUsernameOrPasswordLike(String userName, String passWord);
+	/*public List<User> findByUsernameStartingWith(String userName);
 	public List<User> findByUsernameOrPasswordEndingWith(String userName, String passWord);
 	public List<User> findByUsernameOrPasswordContaining(String userName, String passWord);*/
 
